b64bf99887759f2397df70e47053ff993a409e8d
afmirez <ramirezsilva.andres.felipe@gmail.com>
2025-11-18 11:13:24 -0600

    Home layout was created

--- fennec-ui/src/app/[locale]/(home)/layout.tsx
+++ fennec-ui/src/app/[locale]/(home)/layout.tsx
@@ -1,7 +1,17 @@
+import styles from "./styles.module.css";
+
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className={styles.layout}>
+      <div className={styles.layoutInner}>
+        <header className={styles.header}></header>
+        <main className={styles.content}>{children}</main>
+        <footer className={styles.footer}></footer>
+      </div>
+    </div>
+  );
 }
